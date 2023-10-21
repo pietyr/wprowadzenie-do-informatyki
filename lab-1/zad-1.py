138cca9926225999eec5890114517170281021c5
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-10-21 19:20:21 +0200

    zadanie 1

---
+++ lab-1/zad-1.py
@@ -0,0 +1,14 @@
+from random import *
+
+losowa = randint(1, 10)
+gratrwa = True
+while gratrwa:
+  podana = int(input('Zgadnij liczbe od 1 do 10: '))
+
+  if podana == losowa:
+    print('Wygrales!')
+    gratrwa = False
+  elif podana < losowa:
+    print('Podana liczba jest za mala')
+  else:
+    print('Podana liczba jest za duza')
