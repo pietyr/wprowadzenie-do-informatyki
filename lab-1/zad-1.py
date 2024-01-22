677a40b34f7ca95dbc97395f1cf87e137814beb7
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-01-22 11:30:08 +0100

    build: switch ide to webstorm

--- lab-1/zad-1.py
+++ lab-1/zad-1.py
@@ -3,12 +3,12 @@ from random import *
 losowa = randint(1, 10)
 gratrwa = True
 while gratrwa:
-  podana = int(input('Zgadnij liczbe od 1 do 10: '))
+    podana = int(input('Zgadnij liczbe od 1 do 10: '))
 
-  if podana == losowa:
-    print('Wygrales!')
-    gratrwa = False
-  elif podana < losowa:
-    print('Podana liczba jest za mala')
-  else:
-    print('Podana liczba jest za duza')
+    if podana == losowa:
+        print('Wygrales!')
+        gratrwa = False
+    elif podana < losowa:
+        print('Podana liczba jest za mala')
+    else:
+        print('Podana liczba jest za duza')
